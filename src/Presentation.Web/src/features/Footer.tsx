7d074fe5dd8de176e92bf7e8861bc755b0f46224
mikepowtech <mike.warnes@powtechnology.com>
2025-06-25 09:42:06 +0100

    Update footer text and alignment in AppFooter
    Modified the `Footer` component in the `AppFooter` function.
    Replaced the original text with "Metronview App ©2023 Created by Powtechnology App Team" and changed the text alignment from "center" to "left".

--- src/Presentation.Web/src/features/Footer.tsx
+++ src/Presentation.Web/src/features/Footer.tsx
@@ -2,8 +2,8 @@ import { Footer } from "antd/es/layout/layout";
 
 export const AppFooter = () => {
   return (
-    <Footer style={{ textAlign: "center" }}>
-      Unified App ©2023 Created by Unified App Team
+    <Footer style={{ textAlign: "left" }}>
+      Metronview App ©2023 Created by Powtechnology App Team
     </Footer>
   );
 };
